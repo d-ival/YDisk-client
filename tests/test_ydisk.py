242bc1181ce1e4f8c1518bbdb72d342d565555da
Ivan Danilov <d-ival@yandex.ru>
2020-10-04 23:36:56 +0300

    access-token will be automatically created if not exists

--- tests/test_ydisk.py
+++ tests/test_ydisk.py
@@ -1,10 +1,16 @@
 import unittest
 import ydisk
-
+from os import path
 
 class TestYDiskClient(unittest.TestCase):
     def setUp(self) -> None:
-        with open("ydisk_token.txt") as tokenfile:
+
+        token_file_path = "ydisk_token.txt"
+        if not path.exists(token_file_path):
+            with open(token_file_path, 'tw', encoding='utf-8') as f:
+                f.write('Put here your YandexDisk access-token')
+
+        with open(token_file_path, encoding='utf-8') as tokenfile:
             self.access_token = tokenfile.read().strip()
 
         self.ydisk_client = ydisk.YDiskClient(self.access_token)
